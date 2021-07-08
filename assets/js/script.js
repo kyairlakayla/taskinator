61760975fec233f113dced33d9a4582ddad90c6f
Kayla <kyairlakayla.davis@gmail.com>
2021-07-07 19:03:24 -0500

    add ability to submit custom tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var formEL = document.querySelector("#task-form")
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function(event) {
@@ -6,31 +6,42 @@ var taskFormHandler = function(event) {
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // check if input values are empty strings
+    if (taskNameInput === "" || taskTypeInput === "") {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+  formEl.reset();
+
+   // reset form fields for next task to be entered
+   document.querySelector("input[name='task-name']").value = "";
+   document.querySelector("select[name='task-type']").selectedIndex = 0;
+
     // package up data as an object
     var taskDataObj = {
-    name: taskNameInput,
-    type: taskTypeInput
+        name: taskNameInput,
+        type: taskTypeInput
     };
 
   // send it as an argument to createTaskEl
   createTaskEl(taskDataObj);
 };
 
-var createTaskEl = function(taskData0bj) {
-// create list item 
-var listItemEl = document.createElement("li");
-listItemEl.className = "task-item";
+var createTaskEl = function(taskDataObj) {
+    // create list item 
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
 
-// create div to hold task info and add to list item
-var taskInfoEl = document.createElement("div");
-taskInfoEl.classname = "task-info";
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.classname = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    listItemEl.appendChild(taskInfoEl); 
 
-// add HTML content to div
-taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
-listItemEl.appendChild(taskInfoEl); 
+    console.dir(listItemEl);
 
-// add entire list item to list
-tasksToDoEl.appendChild(listItemEl);
-} 
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
+};
 
-formEL.addEventListener("submit", taskFormHandler);
+formEl.addEventListener("submit", taskFormHandler);
